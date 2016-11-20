c79672afbba89500d58c90882a4f983525c8989d
BuildTools <unconfigured@null.spigotmc.org>
2016-11-20 15:20:30 +0100

    Initial commit

---
+++ app/src/main/java/froststudio/avtobusi3/a1p1Fragment.java
@@ -0,0 +1,21 @@
+package froststudio.avtobusi3;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Darko on 20/11/2016.
+ */
+
+public class a1p1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.avtobus1_layout,null);
+    }
+}
---
+++ app/src/main/java/froststudio/avtobusi3/a1p3Fragment.java
@@ -0,0 +1,17 @@
+package froststudio.avtobusi3;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class a1p3Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.avtobus1_3_layout,null);
+    }
+}
